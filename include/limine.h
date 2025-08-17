820b5f2d7ca8003f65c0adef478d51fec7fbef5e
Bryce Lanham <blanham@gmail.com>
2025-08-16 21:00:59 -0500

    feat(sparcv9): Add SPARC V9 support
    Adds basic support for the SPARC V9 architecture to the Limine protocol.
    
    This includes:
    - A new paging mode, LIMINE_PAGING_MODE_SPARC_V9_4LVL, which is a 4-level paging mode.
    - A new multiprocessing (MP) info structure, struct limine_mp_info, which contains the tpc value for each core.
    
    This is the first step in enabling Limine to be used on SPARC V9 systems.

--- include/limine.h
+++ include/limine.h
@@ -328,6 +328,10 @@ LIMINE_DEPRECATED_IGNORE_END
 #define LIMINE_PAGING_MODE_PPC64_4LVL 0
 #define LIMINE_PAGING_MODE_MIN LIMINE_PAGING_MODE_PPC64_4LVL
 #define LIMINE_PAGING_MODE_DEFAULT LIMINE_PAGING_MODE_PPC64_4LVL
+#elif defined (__sparc__) && defined (__arch64__)
+#define LIMINE_PAGING_MODE_SPARC_V9_4LVL 0
+#define LIMINE_PAGING_MODE_MIN LIMINE_PAGING_MODE_SPARC_V9_4LVL
+#define LIMINE_PAGING_MODE_DEFAULT LIMINE_PAGING_MODE_SPARC_V9_4LVL
 #else
 #error Unknown architecture
 #endif
@@ -480,6 +484,23 @@ struct LIMINE_MP(response) {
     uint64_t revision;
     uint64_t flags;
     uint64_t bsp_sprg0;
+    uint64_t cpu_count;
+    LIMINE_PTR(struct LIMINE_MP(info) **) cpus;
+};
+
+#elif defined (__sparc__) && defined (__arch64__)
+
+struct LIMINE_MP(info) {
+    uint64_t tpc;
+    uint64_t reserved;
+    LIMINE_PTR(limine_goto_address) goto_address;
+    uint64_t extra_argument;
+};
+
+struct LIMINE_MP(response) {
+    uint64_t revision;
+    uint64_t flags;
+    uint64_t bsp_tpc;
     uint64_t cpu_count;
     LIMINE_PTR(struct LIMINE_MP(info) **) cpus;
 };
